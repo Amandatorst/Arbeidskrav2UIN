7be7fd6ca70ae40e84b056ecc4cae15f6a7ce455
Amandatorst <amanda.torstensen@gmail.com>
2026-02-02 15:19:28 +0100

    opprettet komponentene

---
+++ src/assets/components/Shoppinglist.jsx
@@ -0,0 +1,11 @@
+export default function Handlelisten(){
+return (
+        <article className="handleliste">
+            <h2>"Handleliste"</h2>
+            <h3>"Vare"</h3>
+            <h3>"Antall"</h3>
+
+            <button>Done</button>
+        </article>
+    )
+}
